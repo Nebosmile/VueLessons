fe0a469b427666831d166bedfe36721d2ae81988
Pavel <akulov_pavel88@mail.ru>
2017-05-24 19:14:10 +0300

    lessons in process

---
+++ simple/src/Header.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>
+      <header>
+          {{ header }}
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            header: 'I am here!'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ simple/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div>
+      {{ header }}
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            header: 'I am here!'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
